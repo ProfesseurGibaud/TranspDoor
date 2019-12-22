e61391c8629b570ff7c3ebec54287f3e1b0a2569
Professeur Gibaud <mail@professeurgibaud.ovh>
2019-12-22 18:09:18 +0100

    Nouvelle tentative avec un tutorial cette fois

--- TwitterApp.py
+++ TwitterApp.py
@@ -28,7 +28,6 @@ user = 'CecilePrevieu'
 
 query = {'q': 'Storengy',
         'user' : 'user',
-        'count': 100
         }
         
 
@@ -40,15 +39,49 @@ dict_ = {'user': [], 'date': [], 'text': [], 'favorite_count': []}
 for status in twitter.search(**query)['statuses']:
     print(status['user']['screen_name'])
     if status['user']['screen_name'] == user:
-        dict_2['user'].append(status['user']['screen_name'])
-        dict_2['date'].append(status['created_at'])
-        dict_2['text'].append(status['text'])
-        dict_2['favorite_count'].append(status['favorite_count'])    
+        dict_['user'].append(status['user']['screen_name'])
+        dict_['date'].append(status['created_at'])
+        dict_['text'].append(status['text'])
+        dict_['favorite_count'].append(status['favorite_count'])    
 
 # Structure data in a pandas DataFrame for easier manipulation
 
 
-df = pd.DataFrame(dict_2)
+df = pd.DataFrame(dict_)
 df.sort_values(by='favorite_count', inplace=True, ascending=False)
 df.head(5)
 
+
+
+def Recherche(user,keyword,Date):
+    query = {'q': keyword,'until' : Date}
+    
+    #Dictionnaire à modifier avec nom des users liés
+    dict_ = {'user': [], 'date': [], 'text': []}
+    for status in twitter.search(**query)['statuses']:
+        print(status['user']['screen_name'])
+        if status['user']['screen_name'] == user:
+            dict_['user'].append(status['user']['screen_name'])
+            dict_['date'].append(status['created_at'])
+            dict_['text'].append(status['text'])
+            
+    
+    # Structure data in a pandas DataFrame for easier manipulation
+    
+    
+    df = pd.DataFrame(dict_)
+    return df
+
+user = "CecilePrevieu"
+keyword = "Storengy"
+Date = "2019-12-19"
+
+
+"""
+
+Nouvelle tentative (http://2017.compciv.org/guide/topics/python-nonstandard-libraries/twython-guide/twitter-twython-api-basics.html)
+
+"""
+
+
+result = twitter.show_user(screen_name='CecilePrevieu')

3c7e2b73d247715796802c2c1609aa9d3ffec155
FrancoisT <francois@t.com>
2019-12-22 15:47:09 +0100

    Changement DataLoad pour François et Sylvain

--- LectCsvHAVP.py
+++ LectCsvHAVP.py
@@ -6,13 +6,18 @@ import pprint
 def dossier():
     os.chdir("Google Drive//Python//TranspDoor")
 
-def LoadData():
-    os.chdir("..")
-    os.chdir("..")
-    os.chdir("..")
-    os.chdir("VuesFusionnes")
-    DataAction = pd.read_csv("2_actions.csv",sep = ";")
-    DataInfoGenerale = pd.read_csv("1_informations_generales.csv",sep = ";")
-    os.chdir("..")
-    dossier()
+def LoadData(i):
+    if i == 0:
+        os.chdir("..")
+        os.chdir("..")
+        os.chdir("..")
+        os.chdir("VuesFusionnes")
+        DataAction = pd.read_csv("2_actions.csv",sep = ";")
+        DataInfoGenerale = pd.read_csv("1_informations_generales.csv",sep = ";")
+        os.chdir("..")
+        dossier()
+    if i == 1:
+        DataInfoGenerale = pd.read_csv(r"C:\Users\François T\Desktop\Python\TranspDoor data\Vues fusionnées\1_informations_generales.csv",sep = ";")
+        DataAction = pd.read_csv(r"C:\Users\François T\Desktop\Python\TranspDoor data\Vues fusionnées\2_actions.csv",sep = ";")
+        
     return [DataInfoGenerale,DataAction]

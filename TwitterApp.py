ac31dc13e68c237ac97657bb77e3b8cfdf50a2e1
Professeur Gibaud <mail@professeurgibaud.ovh>
2019-12-22 19:35:32 +0100

    Je vais te bombarder hé hé bombarder hé hé (Recherche de tous les tweets ok) (pas de filtres encore))

--- TwitterApp.py
+++ TwitterApp.py
@@ -28,13 +28,30 @@ user = "CecilePrevieu"
 keyword = "Storengy"
 Date = "2019-12-19"
 
+user = "MET_Token"
+
+def ModifListe(L,item):
+    L.append(item)
+    return L
+
 #Mot Clé, DateDebut, Date Din inutiles pour l'instant
 def Recherche(user,MotCle,DateDebut,DateFin):
-    NombreTweets = twitter.show_user(screen_name = "CecilePrevieu")["statuses_count"]
-    if NombreTweets < 200:
-        List = twitter.get_user_timeline(screen_name = user,count = NombreTweets + 1)
-    if NombreTweets >= 200:
-        compteur = NombreTweets
-        while compteur >= 200:
-            compteur = 0 #Fallait finir
+    NombreTweets = twitter.show_user(screen_name = user)["statuses_count"]
+    compteur = NombreTweets
+    Liste = []
+    for tweet in twitter.get_user_timeline(screen_name = user,count = 200):
+        Liste = ModifListe(Liste,tweet)
+    
+    last_id = Liste[len(Liste)-1]['id']
+    
+    while len(Liste) < NombreTweets:
+        print(len(Liste))
+        for item in twitter.get_user_timeline(screen_name = user,max_id = last_id,count = 199):
+            print(item["lang"])
+            Liste = ModifListe(Liste,item)
+        last_id = Liste[len(Liste)-1]['id']
+    
+    return Liste 
         
+        
+   

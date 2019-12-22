f9a212665f0bbfa80bad8c55592f6c11f10918ba
Professeur Gibaud <mail@professeurgibaud.ovh>
2019-12-22 16:40:49 +0100

    Ajout de la fonction dossier dans lecture Python

--- TwitterApp.py
+++ TwitterApp.py
@@ -1,11 +1,19 @@
 from twython import Twython
 import json
 
+import os
+
+
+def dossier():
+    os.chdir("Google Drive//Python//TranspDoor")
+
 
 with open("twitter_credentials.json",'r') as file:
     creds = json.load(file)
     
 python_tweets = Twython(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'])
+
+
 
 query = {'q': 'learn python',
         'result_type': 'popular',
@@ -31,4 +39,32 @@ df.head(5)
 
 
 
+
+
+
+
+
+import time
+from twython import TwythonStreamer
+
+TERMS='social'
+
+APP_KEY='MY APP KEY'
+APP_SECRET='MY APP SECRET'
+OAUTH_TOKEN='MY OATH TOKEN'
+OAUTH_TOKEN_SECRET='MY OATH TOKEN SECRET'
+FOLLOW  = "PREVIEU Cécile" #Personne d'intéret
+
+
+class BlinkyStreamer(TwythonStreamer):
+    def on_success(self, data):
+        if 'text' in data:
+            print(data['text'].encode('utf-8'))
+            
+            
+try:
+        stream = BlinkyStreamer(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+        stream.statuses.filter(track=TERMS,follow = FOLLOW)
+except:
+    KeyboardInterrupt
 

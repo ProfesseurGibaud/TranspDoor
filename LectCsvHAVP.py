6152222471233cd3d2fd91192dc75fc9a1bd8fb2
FrancoisT <francois@t.com>
2019-12-22 18:43:38 +0100

    Ajoute un dataframe avec le nom de l'entreprise et le nom du lobbyist

--- LectCsvHAVP.py
+++ LectCsvHAVP.py
@@ -12,12 +12,16 @@ def LoadData(i):
         os.chdir("..")
         os.chdir("..")
         os.chdir("VuesFusionnes")
-        DataAction = pd.read_csv("2_actions.csv",sep = ";")
-        DataInfoGenerale = pd.read_csv("1_informations_generales.csv",sep = ";")
+        dfAction = pd.read_csv("2_actions.csv",sep = ";")
+        dfInfoGenerale = pd.read_csv("1_informations_generales.csv",sep = ";")
         os.chdir("..")
         dossier()
     if i == 1:
-        DataInfoGenerale = pd.read_csv(r"C:\Users\François T\Desktop\Python\TranspDoor data\Vues fusionnées\1_informations_generales.csv",sep = ";")
-        DataAction = pd.read_csv(r"C:\Users\François T\Desktop\Python\TranspDoor data\Vues fusionnées\2_actions.csv",sep = ";")
+        dfInfoGenerale = pd.read_csv(r"C:\Users\François T\Desktop\Python\TranspDoor data\Vues fusionnées\1_informations_generales.csv",sep = ";")
+        dfAction = pd.read_csv(r"C:\Users\François T\Desktop\Python\TranspDoor data\Vues fusionnées\2_actions.csv",sep = ";")
         
     return [DataInfoGenerale,DataAction]
+dev = 1
+LoadData(dev) 
+
+cyloby = dfInfoGenerale[["denomination","nom_prenom_collaborateur"]].drop_duplicates() #Permet d'avoir une liste des lobbyists attachés à une entreprise

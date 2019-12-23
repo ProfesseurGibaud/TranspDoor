1e511432c8305487800ef94eb268e6018a0d30f9
FrancoisT <francois@t.com>
2019-12-24 00:19:22 +0100

    Creates a prepare_data function that creates one subset of companies and lobbyist and the second of the actions

--- LectCsvHAVP.py
+++ LectCsvHAVP.py
@@ -22,32 +22,41 @@ def LoadData(i):
         
     return [dfInfoGenerale,dfAction]
 
+def prepare_data(dfInfoGenerale,dfAction):
+    cyloby = dfInfoGenerale[["denomination","nom_prenom_collaborateur"]].drop_duplicates() #Permet d'avoir une liste des lobbyists attachés à une entreprise
+
+    #Prépare une sous-liste des actions
+    actionlob = dfAction[["denomination","date_premiere_publication","date_debut","date_fin","date_publication","date_publication_activite","objet_activite","domaines_intervention_actions_menees","action_menee","decision_concernee"]]
+
+    return [cyloby, actionlob]
+
+
 dev = 1
 [dfInfoGenerale,dfAction] = LoadData(dev) 
 
-cyloby = dfInfoGenerale[["denomination","nom_prenom_collaborateur"]].drop_duplicates() #Permet d'avoir une liste des lobbyists attachés à une entreprise
-
-actionlob = dfAction[["denomination","date_premiere_publication","date_debut","date_fin","date_publication","date_publication_activite","objet_activite","domaines_intervention_actions_menees","action_menee","decision_concernee"]]
-
-def ArbreEntrepriseCollab()
-    dict = {}
-    svg = ""
-    for item in cyloby.denomination:
-        if item != svg:
-            dict[item] = []
-            svg = item
-
-    for item in cyloby.iterrows():
-          dict[item[1][0]].append(item[1][1])
-    return dict
-    
-dict = ArbreEntrepriseCollab()
+[cyloby, actionlob] = prepare_data(dfInfoGenerale, dfAction)
 
 
 
-
-
-for i in actionlob:
-    for j in cyloby:
-        if cyloby.denomination == actionlob.denomination:
-            actionlob.
+#def ArbreEntrepriseCollab()
+#    dict = {}
+#    svg = ""
+#    for item in cyloby.denomination:
+#        if item != svg:
+#            dict[item] = []
+#            svg = item
+#
+#    for item in cyloby.iterrows():
+#          dict[item[1][0]].append(item[1][1])
+#    return dict
+#    
+#dict = ArbreEntrepriseCollab()
+#
+#
+#
+#
+#
+#for i in actionlob:
+#    for j in cyloby:
+#        if cyloby.denomination == actionlob.denomination:
+#            actionlob.
